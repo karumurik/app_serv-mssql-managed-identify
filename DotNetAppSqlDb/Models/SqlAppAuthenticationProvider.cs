2a4055ec3609be3f13d02c3541d2c29fb79ed35a
ananda <myyskool@myyskool>
2020-12-13 00:08:59 +0000

    Initial Commit

---
+++ DotNetAppSqlDb/Models/SqlAppAuthenticationProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DotNetAppSqlDb.Models
+{
+    public class SqlAppAuthenticationProvider: SqlAuthenticationProvider
+    {
+        private static readonly AzureServiceTokenProvider _tokenProvider = new AzureServiceTokenProvider();
+
+        public override async Task<SqlAuthenticationToken> AcquireTokenAsync(SqlAuthenticationParameters parameters)
+        {
+            var authResult = await _tokenProvider.GetAuthenticationResultAsync("https://database.windows.net/").ConfigureAwait(false);
+
+            return new SqlAuthenticationToken(authResult.AccessToken, authResult.ExpiresOn);
+        }
+
+        public override bool IsSupported(SqlAuthenticationMethod authenticationMethod)
+        {
+            return authenticationMethod == SqlAuthenticationMethod.ActiveDirectoryInteractive;
+        }
+    }
+}
